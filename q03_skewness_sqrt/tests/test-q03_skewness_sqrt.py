cb68c8bd1e131c37270aa3c98069f68da84cc0b9
ABHISHEK THOMBRE <ug201314001@iitj.ac.in>
2017-10-17 15:42:34 +0530

    Update test-q03_skewness_sqrt.py

--- q03_skewness_sqrt/tests/test-q03_skewness_sqrt.py
+++ q03_skewness_sqrt/tests/test-q03_skewness_sqrt.py
@@ -24,6 +24,6 @@ class TestSkewness_sqrt(TestCase):
                                   type(skewed_sqrt_val2)))
 
         # Return value tests
-        self.assertEqual(skewed_sqrt_val1, 0.5936439161879563, "Return value does not match expected value")
-        self.assertEqual(skewed_sqrt_val2, 0.9421834681211159, "Return value does not match expected value")
+        self.assertEqual(skewed_sqrt_val1, 0.59364, "Return value does not match expected value")
+        self.assertEqual(skewed_sqrt_val2, 0.94218, "Return value does not match expected value")
 
